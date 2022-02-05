a789e180037bed5dabaf7ec0ceb9f007f5e7c5c3
nzulaerastus@gmail.com <nzulaerastus@gmail.com>
2022-02-05 10:40:54 +0300

    changes.

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -5,3 +5,12 @@ function changeImage(event){
     mainImage.src= event.target.src
 }
 window.onload = init;
+
+function getFooter(){
+    'use strict'
+    if (document && document.getElementById){
+        const footerEnd = document.getElementById('footer');
+        footerEnd.innerHTML = "Copyright &copy " + new Date().getFullYear();
+    }
+}
+getFooter()
